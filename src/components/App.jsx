7835b80916b6ce6a9e312e288685cf6f148ca795
Leskovets Maksym <leskovecmaksim@gmail.com>
2024-04-11 23:00:39 +0200

    finished hw 3 (part 1)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,17 +4,28 @@ import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import { SecondaryTitle, Title } from './App.styles';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const localData = localStorage.getItem('contacts');
+    if (localData && JSON.parse(localData).length > 0) {
+      this.setState({ contacts: JSON.parse(localData) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts.length !== contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
 
   addNewContact = newContact => {
     const { contacts } = this.state;
@@ -29,6 +40,10 @@ export class App extends Component {
     this.setState(({ contacts }) => ({
       contacts: [newContact, ...contacts],
     }));
+
+    toast.success('Element added successfully!', {
+      position: 'top-right',
+    });
   };
 
   searchContact = e => {
@@ -47,6 +62,14 @@ export class App extends Component {
     this.setState(({ contacts }) => ({
       contacts: contacts.filter(contact => contact.id !== id),
     }));
+
+    toast.success('Element deleted successfully!', {
+      position: 'top-right',
+      style: {
+        backgroundColor: '#ffcc00',
+        color: '#333',
+      },
+    });
   };
 
   render() {
@@ -57,6 +80,7 @@ export class App extends Component {
 
         <SecondaryTitle>Contacts</SecondaryTitle>
         <Filter onSearch={this.searchContact} />
+        <ToastContainer />
         <ContactList
           data={this.filterContacts()}
           handleDelete={this.deleteContact}
